6e3e7311bd99c09f32dd03e96699aee71a674247
yelayou <yelayou@outlook.com>
2024-01-25 01:45:51 -0500

    Initial Commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
-import { Routes } from '@angular/router';
+//import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+//export const routes: Routes = [];
+
+
+// app-routing.module.ts
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ResumeComponent } from './resume/resume.component';
+
+export const routes: Routes = [
+  { path: 'resume', component: ResumeComponent },
+  { path: '', redirectTo: '/resume', pathMatch: 'full' },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
